50f08c3d98db5e9539c35d7f75342777ebe93f7d
Даниил Куксарь <dan.kukar@yandex.ru>
2024-07-14 18:56:25 +0300

    + test на получение списка подзадач по эндпоинту /epics/{id}/subtasks

--- src/service/exceptions/OverlappingTasksTimeException.java
+++ src/service/exceptions/OverlappingTasksTimeException.java
@@ -4,4 +4,8 @@ public class OverlappingTasksTimeException extends RuntimeException {
     public OverlappingTasksTimeException() {
         super("Tasks are overlapping");
     }
+
+    public OverlappingTasksTimeException(String message) {
+        super(message);
+    }
 }
